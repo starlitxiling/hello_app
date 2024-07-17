0c027f3d59f166c81d8a44b544a5ca7d98bb7ea3
1754165401 <1754165401@qq.com>
2024-07-17 22:11:40 +0800

    add loop app

---
+++ src/app3/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+static mut COUNTER: u32 = 0;
+
+#[no_mangle]
+pub unsafe extern "C" fn _start() -> ! {
+    loop {
+        COUNTER += 1;
+    }
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
